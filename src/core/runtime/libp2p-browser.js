fa078ad7c279d02e606fa4a111edbc5ed1806200
E1Code <39209383+E1Code@users.noreply.github.com>
2018-12-17 17:44:39 -0800

    ethoFS Private Network Update

--- src/core/runtime/libp2p-browser.js
+++ src/core/runtime/libp2p-browser.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var fs = require('fs');
 const WS = require('libp2p-websockets')
 const WebRTCStar = require('libp2p-webrtc-star')
 const WebSocketStar = require('libp2p-websocket-star')
@@ -7,15 +7,18 @@ const Multiplex = require('libp2p-mplex')
 const SECIO = require('libp2p-secio')
 const Bootstrap = require('libp2p-bootstrap')
 const libp2p = require('libp2p')
+const Protector = require('libp2p-pnet')
 const defaultsDeep = require('@nodeutils/defaults-deep')
 
 class Node extends libp2p {
   constructor (_options) {
-    const wrtcstar = new WebRTCStar({ id: _options.peerInfo.id })
-    const wsstar = new WebSocketStar({ id: _options.peerInfo.id })
-
+    const wrtcstar = new WebRTCStar({id: _options.peerInfo.id})
+    const wsstar = new WebSocketStar({id: _options.peerInfo.id})
+    const swarmKey = "/key/swarm/psk/1.0.0/\n/base16/\n38307a74b2176d0054ffa2864e31ee22d0fc6c3266dd856f6d41bddf14e2ad63";
+    var swarmKeyBuffer = new Buffer(swarmKey);
     const defaults = {
       modules: {
+        connProtector: new Protector(swarmKeyBuffer),
         transport: [
           WS,
           wrtcstar,
@@ -46,8 +49,8 @@ class Node extends libp2p {
           }
         },
         EXPERIMENTAL: {
-          dht: false,
-          pubsub: false
+          dht: true,
+          pubsub: true
         }
       }
     }
