aa3521f716c8b87c42ec0a565d9cbfad6a490076
E1Code <39209383+E1Code@users.noreply.github.com>
2018-12-17 17:40:36 -0800

    ethoFS Bootnode Updates

--- src/core/runtime/config-browser.js
+++ src/core/runtime/config-browser.js
@@ -3,6 +3,14 @@
 module.exports = () => ({
   Addresses: {
     Swarm: [
+     '/dns4/wss1.ethofs.com/tcp/443/wss/ipfs/QmTFUcUuMSN7KLytjtqnHCjixqd4ig3PrSbdQ2mW9Q8qeY',
+     '/dns4/wss.ethofs.com/tcp/443/wss/ipfs/QmV856mLWnTDaj5LQvS3dCa3qjz4DNC9cKQJNSrwtqcHzT',
+     '/dns4/wss2.ethofs.com/tcp/443/wss/ipfs/QmdEs4acs8eiAgYr7TFaF43k1BMtGZdF5HkSz9CH4YTVnL',
+     '/dns4/wss3.ethofs.com/tcp/443/wss/ipfs/QmSfEKCzPWA6MmG2ZLK4Vqnq6oB6rvrLyUpHdNqng5nQ4t',
+     '/dns4/wss4.ethofs.com/tcp/443/wss/ipfs/QmeG81bELkgLBZFYZc53ioxtvRS8iNVzPqxUBKSuah2rcQ',
+     '/dns4/wss5.ethofs.com/tcp/443/wss/ipfs/QmRwQ49Zknc2dQbywrhT8ArMDS9JdmnEyGGy4mZ1wDkgaX',
+     '/dns4/wss6.ethofs.com/tcp/443/wss/ipfs/QmTZsBNb7dfJJmwuAdXBjKZ7ZH6XbpestZdURWGJVyAmj2'
+
     ],
     API: '',
     Gateway: ''
@@ -17,13 +25,14 @@ module.exports = () => ({
     }
   },
   Bootstrap: [
-    '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
-    '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
-    '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
-    '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
-    '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
-    '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
-    '/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
-    '/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6'
+    '/dns4/wss1.ethofs.com/tcp/443/wss/ipfs/QmTFUcUuMSN7KLytjtqnHCjixqd4ig3PrSbdQ2mW9Q8qeY',
+    '/dns4/wss.ethofs.com/tcp/443/wss/ipfs/QmV856mLWnTDaj5LQvS3dCa3qjz4DNC9cKQJNSrwtqcHzT',
+    '/dns4/wss2.ethofs.com/tcp/443/wss/ipfs/QmdEs4acs8eiAgYr7TFaF43k1BMtGZdF5HkSz9CH4YTVnL',
+    '/dns4/wss3.ethofs.com/tcp/443/wss/ipfs/QmSfEKCzPWA6MmG2ZLK4Vqnq6oB6rvrLyUpHdNqng5nQ4t',
+    '/dns4/wss4.ethofs.com/tcp/443/wss/ipfs/QmeG81bELkgLBZFYZc53ioxtvRS8iNVzPqxUBKSuah2rcQ',
+    '/dns4/wss5.ethofs.com/tcp/443/wss/ipfs/QmRwQ49Zknc2dQbywrhT8ArMDS9JdmnEyGGy4mZ1wDkgaX',
+    '/dns4/wss6.ethofs.com/tcp/443/wss/ipfs/QmTZsBNb7dfJJmwuAdXBjKZ7ZH6XbpestZdURWGJVyAmj2'
+
   ]
 })
+
